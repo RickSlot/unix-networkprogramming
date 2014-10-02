a16a5342148ed2936ad8c3d21ea4b3ff28da7781
Tony Abidi <leaw@live.nl>
2014-10-02 11:05:35 +0200

    Code cleanup

--- pQI6ZP/default/controller.php
+++ pQI6ZP/default/controller.php
@@ -1,28 +1,25 @@
 <?php 
 // public function generate_table(){
-	$con=mysqli_connect("localhost","root","123","sniffer");
+$con=mysqli_connect("localhost","root","123","sniffer");
 // Check connection
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
-	$result = mysqli_query($con,"SELECT * FROM packets");
-		$only_once = true;
-	while($row = mysqli_fetch_array($result)) {
-		// echo "<tr><td>";
-		echo "<tr>";
-		foreach ($row as $value) {
-			if ($only_once) {
-				echo "<td>" . $value . "</td>";	
-				$only_once = false;
-			}else{
-				$only_once = true;
-			}
+$result = mysqli_query($con,"SELECT * FROM packets");
+$only_once = true;
+while($row = mysqli_fetch_array($result)) {
+	echo "<tr>";
+	foreach ($row as $value) {
+		if ($only_once) {
+			echo "<td>" . $value . "</td>";	
+			$only_once = false;
+		}else{
+			$only_once = true;
 		}
-		// echo $row['dest_mac'] . "</td><td>" . $row['src_mac'] . "</td><td>" . $row['interface_protocol'] . "</td><td>" . $row['version'] . "</td><td>" . $row['ip_hdr_lngt'] . "</td><td>" . $row['ttl'] . "</td><td>" . $row['ip_protocol'] . "</td><td>" . $row['src_adress'] . "</td><td>" . $row['dest_adress'] . "</td><td>" . $row['src_port'] . "</td><td>" . $row['dest_port'] . "</td><td>" . $row['seq_num'] . "</td><td>" . $row['acknowledgement'] . "</td><td>" . $row['tcp_header_lngt'];
-		// echo "</td></tr>";
-		echo "</tr>";
 	}
+	echo "</tr>";
+}
 
-	mysqli_close($con);	
+mysqli_close($con);	
 // }
