b308002dcd057d8da718394846e3bb2c08a4946f
Tony <tony@terminalvision.nl>
2014-10-15 10:19:38 +0000

    work in progress

--- code/server.py
+++ code/server.py
@@ -23,33 +23,27 @@ def main(argv):
         	print d
     if len(adapterList) != 1:
     	dev = raw_input("Enter adapter name : ")
-   	else: 
-   		dev = adapterList[0]
-   		
-    print "Sniffing device " + dev
-     
-    '''
-    open device
-    # Arguments here are:
-    #   device
-    #   snaplen (maximum number of bytes to capture _per_packet_)
-    #   promiscious mode (1 for true)
-    #   timeout (in milliseconds)
-    '''
+    else: 
+   	dev = adapterList[0]
+    # open selected device     
     cap = pcapy.open_live(dev , 65536 , 1 , 0)
  
-    #start sniffing packets
-    while(1) :
-        (header, packet) = cap.next()
-        #print ('%s: captured %d bytes, truncated to %d bytes' %(datetime.datetime.now(), header.getlen(), header.getcaplen()))
-        safe_packet(packet)
+    # start packets sniffing
+    try:
+        while(True) :
+            (header, packet) = cap.next()
+            safe_packet(packet)
+    except KeyboardInterrupt:
+        cur.close()
+        db.close()
+	sys.exit(1)
  
-#Convert a string of 6 characters of ethernet address into a dash separated hex string
+#  convert string to mac address
 def eth_addr (a) :
     b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
     return b
  
-#function to parse a packet
+# handel packet
 def safe_packet(packet) :     
     #parse ethernet header
     eth_length = 14
@@ -57,7 +51,6 @@ def safe_packet(packet) :
     eth_header = packet[:eth_length]
     eth = unpack('!6s6sH' , eth_header)
     eth_protocol = socket.ntohs(eth[2])
-    #print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
  
     #Parse IP packets, IP Protocol number = 8
     if eth_protocol == 8 :
@@ -78,9 +71,7 @@ def safe_packet(packet) :
         protocol = iph[6]
         s_addr = socket.inet_ntoa(iph[8]);
         d_addr = socket.inet_ntoa(iph[9]);
- 
-        #print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
- 
+  
         #TCP protocol
         if protocol == 6 :
             t = iph_length + eth_length
@@ -97,9 +88,7 @@ def safe_packet(packet) :
             tcph_length = doff_reserved >> 4
             
 
-            #print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
-            # Use all the SQL you like
-            if source_port != 22 and dest_port != 22:
+            if source_port != 22 and dest_port != 22: # filter ssh traffic 
                 print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
                 print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
                 print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length)
@@ -107,10 +96,8 @@ def safe_packet(packet) :
                 try:
                     # Execute the SQL command
                     cur.execute("INSERT INTO packets(dest_mac, src_mac, interface_protocol, version, ip_hdr_lngt, ttl, ip_protocol, src_adress, dest_adress, src_port, dest_port, seq_num, acknowledgement, tcp_header_lngt) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (eth_addr(packet[0:6]), eth_addr(packet[6:12]), str(eth_protocol), str(version), str(ihl), str(ttl), str(protocol), str(s_addr), str(d_addr), str(source_port), str(dest_port), str(sequence), str(acknowledgement), str(tcph_length)))
-                    # Commit your changes in the database
                     db.commit()
                 except:
-                    # Rollback in case there is any error
                     print 'exception'
                     db.rollback()
              
@@ -119,8 +106,7 @@ def safe_packet(packet) :
              
             #get data from the packet
             data = packet[h_size:]
-             
-            #print 'Data : ' + data
+     
         else :
             print 'protocol is other'
                   
