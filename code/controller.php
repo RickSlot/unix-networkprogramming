5742756ef919cc697c203d051abcc38936e86be7
Rick <rick.slot@trifork.nl>
2014-10-23 11:39:18 +0200

    added udp support

--- code/controller.php
+++ code/controller.php
@@ -6,29 +6,52 @@ if (mysqli_connect_errno()) {
 }
 // print_r($_POST);
 // die();
-$result = mysqli_query($con,"SELECT * FROM packets WHERE `id` >= " . $_POST['id'] . " ORDER BY id DESC LIMIT 50");//LIMIT 50
+$resulttcp = mysqli_query($con,"SELECT * FROM packets WHERE `id` >= " . $_POST['id'] . " ORDER BY id DESC LIMIT 50");//LIMIT 50
+$resultudp = mysqli_query($con,"SELECT * FROM packets WHERE `id` >= " . $_POST['id'] . " ORDER BY id DESC LIMIT 50");//LIMIT 50
 $id = null;
-$data = "";
+$datatcp = "";
+$dataudp = "";
 $only_once = true;
-while($row = mysqli_fetch_array($result)) {
+
+while($row = mysqli_fetch_array($resulttcp)) {
 	if($id == null) {
 		$id = $row['id'];
 	}
 
-	$data .= "<tr>";
+	$datatcp .= "<tr>";
 	foreach ($row as $value) {
 		if ($only_once) {
-			$data .= "<td>" . $value . "</td>";	
+			$datatcp .= "<td>" . $value . "</td>";	
 			$only_once = false;
 		}else{
 			$only_once = true;
 		}
 	}
-	$data .= "</tr>";
+	$datatcp .= "</tr>";
 }
+
+while($row = mysqli_fetch_array($resultudp)) {
+	if($id == null) {
+		$id = $row['id'];
+	}
+
+	$dataudp .= "<tr>";
+	foreach ($row as $value) {
+		if ($only_once) {
+			$dataudp .= "<td>" . $value . "</td>";	
+			$only_once = false;
+		}else{
+			$only_once = true;
+		}
+	}
+	$dataudp .= "</tr>";
+}
+
+
 $json = array();
 $json['id'] = $id;
-$json['data'] = $data;
+$json['datatcp'] = $datatcp;
+$json['dataudp'] = $dataudp;
 echo json_encode($json);
 
 mysqli_close($con);
